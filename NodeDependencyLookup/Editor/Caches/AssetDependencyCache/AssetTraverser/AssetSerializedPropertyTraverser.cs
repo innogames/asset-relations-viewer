ae59376b7311c92bde5f7cf6deefb4c1c2a64dc7
hanschristian.kientopf <hanschristian.kientopf@innogames.com>
2022-02-13 10:58:32 +0100

    Fixed compile error

--- NodeDependencyLookup/Editor/Caches/AssetDependencyCache/AssetTraverser/AssetSerializedPropertyTraverser.cs
+++ NodeDependencyLookup/Editor/Caches/AssetDependencyCache/AssetTraverser/AssetSerializedPropertyTraverser.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
-using DG.DemiEditor;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Profiling;
